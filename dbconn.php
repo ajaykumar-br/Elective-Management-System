7e9f041bb3bbec5b45ede70e13df74063b8d7886
Ajay Kumar B R <44740988+ajaykumar-br@users.noreply.github.com>
2020-01-01 19:40:07 +0530

    Add files via upload

---
+++ dbconn.php
@@ -0,0 +1,4 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'pems');
+if ($conn == false) echo "Connection Lost!";
---
+++ syllabus/or.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>	
+
+<div>
+
+
+<div>
+  <h1>OPERATIONS RESEARCH</h1>
+  <p>
+  					<h2>Course objectives:</h2>
+  							<br> -&gt;· Formulate optimization problem as a linear programming problem.
+<br> -&gt; Solve optimization problems using simplex method.
+<br> -&gt;Formulate and solve transportation and assignment problems.
+<br> -&gt; Apply game theory for decision making problems.
+
+							
+						</p>
+  <hr>
+
+  <p>
+
+
+  	<div>
+  		Module – 1 
+
+		Introduction, Linear Programming: Introduction: The origin, nature and
+impact of OR; Defining the problem and gathering data; Formulating a
+mathematical model; Deriving solutions from the model; Testing the model;
+Preparing to apply the model; Implementation .
+Introduction to Linear Programming Problem (LPP): Prototype example,
+Assumptions of LPP, Formulation of LPP and Graphical method various
+examples.
+
+	</div>
+
+
+	<div>
+  		Module – 2
+  		Simplex Method – 1: The essence of the simplex method; Setting up the simplex
+method; Types of variables, Algebra of the simplex method; the simplex method
+in tabular form; Tie breaking in the simplex method, Big M method, Two phase
+method.
+	</div>
+
+
+	<div>
+		Module-3
+  		Duality Theory - The essence of duality theory, Primal
+dual relationship, conversion of primal to dual problem and vice versa. The dual
+simplex method.
+
+	</div>
+
+	<div>
+  		Module-4
+  		Transportation and Assignment Problems: The transportation problem, Initial
+Basic Feasible Solution (IBFS) by North West Corner Rule method, Matrix
+Minima Method, Vogel’s Approximation Method. Optimal solution by Modified
+Distribution Method (MODI). The Assignment problem; A Hungarian algorithm
+for the assignment problem. Minimization and Maximization varieties in
+transportation and assignment problems.
+	</div>
+
+	<div>
+  		Module – 5
+Game Theory: Game Theory: The formulation of two persons, zero sum games;
+saddle point, maximin and minimax principle, Solving simple games- a prototype
+example; Games with mixed strategies; Graphical solution procedure.
+Metaheuristics: The nature of Metaheuristics, Tabu Search, Simulated
+Annealing, Genetic Algorithms.
+	</div>
+
+<hr>
+
+
+<p>
+  					<h2>Course outcomes:</h2>
+<br>· Select and apply optimization techniques for various problems.
+<br>· Model the given problem as transportation and assignment problem and solve.
+<br>· Apply game theory for decision support system.
+						</p>
+
+ 
+
+
+
+
+
+
+
+</p>
+  <a href="https://www.youtube.com/watch?v=4EUAnzLkHFU" target="_blank">Introducton to Operations Research</a>
+</div>
+
+</div>
+
+
+</body></html>
